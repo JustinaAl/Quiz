7432c2cbfb8569835ea9484c16a67fe90479c43f
JustinaAl <juste.paskeviciute@gmail.com>
2024-12-05 20:05:27 +0100

    First version

--- script.js
+++ script.js
@@ -1,104 +1,106 @@
 let questionsAndAnswers=[{ 
-        question:"1. Puff pastry is made with yeast.",
-        a:"True",
-        b:"False"
-    },{ 
-        question:"2. Baklava is made with puff pastry.",
-        a:"True",
-        b:"False"
-    },{
-        question:"3. An éclair is traditionally filled with whipped cream",
-        a:"True",
-        b:"False"
-    },{
-        question:"4. Tiramisu is French",
-        a:"True",
-        b:"False"
-    },{
-        question:"5. Croissants are made with a laminated dough.",
-        a:"True",
-        b:"False"
-    },{ 
-        question:"6. Which pastry is known for its flaky, buttery layers?",
-        a:"a) Shortcrust pastry",
-        b:"b) Phyllo pastry",
-        c:"c) Puff pastry",
-        d:"d) Choux pastry"
-    },{ 
-        question:"7. Which ingredient is NOT typically used in a traditional French éclair?",
-        a:"a) Choux pastry",
-        b:"b) Vanilla custard",
-        c:"c) Chocolate glaze",
-        d:"d) Almond flour"
-    },{
-        question:"8. Which type of pastry is often used for making tarts?",
-        a:"a) Filo pastry",
-        b:"b) Choux pastry",
-        c:"c) Shortcrust pastry",
-        d:"d) Puff pastry"
-    },{
-        question:"9. What is the key ingredient in puff pastry that creates its flakiness?",
-        a:"a) Baking powder",
-        b:"b) Yeast",
-        c:"c) Butter",
-        d:"d) Milk"
-    },{
-        question:"10. What is the other name for Napoleon cake?",
-        a:"a) Angel food cake",
-        b:"b) Mille-feuille",
-        c:"c) Black Forest",
-        d:"d) Ensaimada"
-    },{ 
-        question:"11. Which of the following are types of laminated dough?",
-        a:"a) Croissant",
-        b:"b) Puff pastry",
-        c:"c) Shortcrust pastry",
-        d:"d) Danish pastry"
-    },{ 
-        question:"12. Which ingredients are typically used in making choux pastry?",
-        a:"a) Flour",
-        b:"b) Butter",
-        c:"c) Eggs",
-        d:"d) Yeast"
-    },{
-        question:"13. Which of these pastries are often filled with frangipane (almond cream)?",
-        a:"a) Galette des Rois",
-        b:"b) Tarte Tatin",
-        c:"c) Almond croissant",
-        d:"d) Bakewell tart"
-    },{
-        question:"14. Which of these pastries are made using yeast dough?",
-        a:"a) Brioche",
-        b:"b) Choux Pastry",
-        c:"c) Pain au chocolat",
-        d:"d) Churros"
-    },{
-        question:"15. Which of the following are classic French pastries?",
-        a:"a) Éclair",
-        b:"b) Tarte Tatin",
-        c:"c) Cannoli",
-        d:"d) Madeleines"
-    }]
+    question:"1. Puff pastry is made with yeast.",
+    a:"True",
+    b:"False"
+},{ 
+    question:"2. Baklava is made with puff pastry.",
+    a:"True",
+    b:"False"
+},{
+    question:"3. An éclair is traditionally filled with whipped cream",
+    a:"True",
+    b:"False"
+},{
+    question:"4. Tiramisu is French",
+    a:"True",
+    b:"False"
+},{
+    question:"5. Croissants are made with a laminated dough.",
+    a:"True",
+    b:"False"
+},{ 
+    question:"6. Which pastry is known for its flaky, buttery layers?",
+    a:"a) Shortcrust pastry",
+    b:"b) Phyllo pastry",
+    c:"c) Puff pastry",
+    d:"d) Choux pastry"
+},{ 
+    question:"7. Which ingredient is NOT typically used in a traditional French éclair?",
+    a:"a) Choux pastry",
+    b:"b) Vanilla custard",
+    c:"c) Chocolate glaze",
+    d:"d) Almond flour"
+},{
+    question:"8. Which type of pastry is often used for making tarts?",
+    a:"a) Filo pastry",
+    b:"b) Choux pastry",
+    c:"c) Shortcrust pastry",
+    d:"d) Puff pastry"
+},{
+    question:"9. What is the key ingredient in puff pastry that creates its flakiness?",
+    a:"a) Baking powder",
+    b:"b) Yeast",
+    c:"c) Butter",
+    d:"d) Milk"
+},{
+    question:"10. What is the other name for Napoleon cake?",
+    a:"a) Angel food cake",
+    b:"b) Mille-feuille",
+    c:"c) Black Forest",
+    d:"d) Ensaimada"
+},{ 
+    question:"11. Which of the following are types of laminated dough?",
+    a:"a) Croissant",
+    b:"b) Puff pastry",
+    c:"c) Shortcrust pastry",
+    d:"d) Danish pastry"
+},{ 
+    question:"12. Which ingredients are typically used in making choux pastry?",
+    a:"a) Flour",
+    b:"b) Butter",
+    c:"c) Eggs",
+    d:"d) Yeast"
+},{
+    question:"13. Which of these pastries are often filled with frangipane (almond cream)?",
+    a:"a) Galette des Rois",
+    b:"b) Tarte Tatin",
+    c:"c) Almond croissant",
+    d:"d) Bakewell tart"
+},{
+    question:"14. Which of these pastries are made using yeast dough?",
+    a:"a) Brioche",
+    b:"b) Choux Pastry",
+    c:"c) Pain au chocolat",
+    d:"d) Churros"
+},{
+    question:"15. Which of the following are classic French pastries?",
+    a:"a) Éclair",
+    b:"b) Tarte Tatin",
+    c:"c) Cannoli",
+    d:"d) Madeleines"
+}];
 
 let body = document.querySelector("body");
 
 let i = 0;
-let mainContainer = document.querySelector(".mainContainer")
+let mainContainer = document.querySelector(".mainContainer");
 
 let rightAnswers =["False","False","False","False","True","c) Puff pastry","d) Almond flour","c) Shortcrust pastry","c) Butter","b) Mille-feuille",["a) Croissant","b) Puff pastry","d) Danish pastry"],["a) Flour", "b) Butter", "c) Eggs"],["a) Galette des Rois", "c) Almond croissant", "d) Bakewell tart"],["a) Brioche", "c) Pain au chocolat"],["a) Éclair", "b) Tarte Tatin", "d) Madeleines"]];
 let chechedValues =[];
 
+let darkLightInput = document.querySelector("#darkLightInput");
+
 
 //Function that collects answers.
 function collectAnswers (){
-    let checked = document.querySelectorAll('input:checked');
+    let checked = document.querySelectorAll('input:not(#darkLightInput):checked');
     if (checked.length < 2) {
         checked.forEach(value => {
-            chechedValues.push(value.value)
+            chechedValues.push(value.value);
         });
     }else{
         let allValues = [];
-        checked.forEach(value=>allValues.push(value.value))
+        checked.forEach(value=>allValues.push(value.value));
         chechedValues.push(allValues);
     }
     
@@ -141,31 +143,31 @@ function theResults(){
             </div>
         </div>
         <div class="allQuestions">
-        </div>`
+        </div>`;
 
 
-        //Prints out all the questions
-        let allQuestions = document.querySelector(".allQuestions");
+    //Prints out all the questions
+    let allQuestions = document.querySelector(".allQuestions");
 
-        questionsAndAnswers.forEach((question, x )=> {
-            let questionBox = document.createElement('div');
-            questionBox.classList.add("questionBox");
+    questionsAndAnswers.forEach((question, x )=> {
+        let questionBox = document.createElement('div');
+        questionBox.classList.add("questionBox");
 
-            let aQuestion = document.createElement('p');
-            aQuestion.classList.add("aQuestion"); 
-            aQuestion.textContent = question.question;
+        let aQuestion = document.createElement('p');
+        aQuestion.classList.add("aQuestion"); 
+        aQuestion.textContent = question.question;
 
 
-            questionBox.append(aQuestion);
+        questionBox.append(aQuestion);
 
-            let correctAnswer = document.createElement("p");
-            correctAnswer.classList.add("rightAnswer");
-            correctAnswer.innerHTML = `Correct answer: ${rightAnswers[x]}
-            <p id="chosenAnswer">Your answer: ${chechedValues[x]}</p><br><br>`
-            questionBox.append(correctAnswer);
-            
-            allQuestions.append(questionBox);
-        });
+        let correctAnswer = document.createElement("p");
+        correctAnswer.classList.add("rightAnswer");
+        correctAnswer.innerHTML = `Correct answer: ${rightAnswers[x]}
+        <p id="chosenAnswer">Your answer: ${chechedValues[x]}</p><br><br>`;
+        questionBox.append(correctAnswer);
+        
+        allQuestions.append(questionBox);
+    });
 
 
 }
@@ -197,11 +199,11 @@ function createAnswerButton(){
             
             buttonContainer.append(answerButtonWrap);
         }
-        });
+    });
 
         //dark light mode thing
         if(darkLightInput.checked){
-            document.querySelectorAll('div.answerButtonWrap .label').forEach(label =>{label.classList.add("light")})
+            document.querySelectorAll('div.answerButtonWrap .label').forEach(label =>{label.classList.add("light")});
         }
 
 
@@ -213,7 +215,7 @@ function createAnswerButton(){
                 document.querySelectorAll(".answerButtonWrap .label").forEach(label => label.classList.remove("light"));
             }
     
-        })
+        });
     
 
 }
@@ -226,9 +228,9 @@ function createNewQuestion(questionText) {
             <p>${questionText}</p>
         </div>
         <div class="answerAndButton">
-            <button class="previousButton"><img src="triangle-svgrepo-com-purple.png" alt="" width="100%"></button>
+            <button class="previousButton"><img src="images/previous.svg" alt="" width="100%"></button>
             <div class="answerContainer"> </div>
-            <button class="nextButton" ><img class="nextButtonImg" src="triangle-svgrepo-com-purple.png" width="100%"  alt=""></button>
+            <button class="nextButton" ><img class="nextButtonImg" src="images/next.svg" width="100%"  alt=""></button>
         </div>`;
     
     body.append(mainContainer);
@@ -236,7 +238,8 @@ function createNewQuestion(questionText) {
     createAnswerButton();
 
     document.querySelector('.nextButton').addEventListener('click',() => {
-        let isChecked = document.querySelector("input:checked");
+        
+        let isChecked = document.querySelector("input:not(#darkLightInput):checked");
 
         if(isChecked){
             collectAnswers ();
@@ -251,7 +254,7 @@ function createNewQuestion(questionText) {
                 mainContainer.innerHTML = "";
                 let submit = document.createElement("button");
                 submit.classList.add("submit");
-                submit.textContent = "Submit the answers"
+                submit.textContent = "Submit the answers";
                 mainContainer.append(submit);
 
                 if(darkLightInput.checked){
@@ -260,10 +263,10 @@ function createNewQuestion(questionText) {
 
 
                 submit.addEventListener("click",() => {
-                    theResults()
+                    theResults();
 
                     //Colors red or green
-                    let allPoinst = rightAnswers.flat().length
+                    let allPoinst = rightAnswers.flat().length;
                     let procent = points/allPoinst * 100;
                     let divScore = document.querySelectorAll('div.score');
                     divScore.forEach(div =>{
@@ -274,10 +277,11 @@ function createNewQuestion(questionText) {
                         }else{
                             div.classList.add("green");
                         }
-                    })
-                })
+                    });
+                });
             }
             }
+    
 
             
     });
@@ -295,21 +299,21 @@ function createNewQuestion(questionText) {
 
 //Starts the game
 document.querySelector("#startGame").addEventListener("click",()=>{
+    if(darkLightInput.checked){
+        document.querySelector('#startGame').classList.remove("light");
+    }
     document.querySelector(".mainContainer").classList.remove("first");
     createNewQuestion(questionsAndAnswers[i].question);
-})
+});
 
 
 
 //Dark light mode button
-
-let darkLightInput = document.querySelector("#darkLightInput");
 darkLightInput.addEventListener("change",()=>{
     if(darkLightInput.checked){
         body.classList.add("light");
         document.querySelector('header').classList.add("light");
         document.querySelector('.mainContainer').classList.add("light");
-        document.querySelector('#startGame').classList.add("light");
     }
     else{
         body.classList.remove("light");
@@ -317,5 +321,5 @@ darkLightInput.addEventListener("change",()=>{
         document.querySelector('.mainContainer').classList.remove("light");
         document.querySelector('#startGame').classList.remove("light");
     }
-})
+});
 
